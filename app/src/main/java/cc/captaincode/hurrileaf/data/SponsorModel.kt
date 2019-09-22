1d0440585e6ff527a34cebe63033ed235a522368
Aoimusha <ldj412@gmail.com>
2019-09-21 23:16:31 -0400

    Registration ViewModel implemented

---
+++ app/src/main/java/cc/captaincode/hurrileaf/data/SponsorModel.kt
@@ -0,0 +1,17 @@
+package cc.captaincode.hurrileaf.data
+
+data class Sponsor
+    (
+    var ID: String = "",
+    var name: String = "",
+    var budget: Int = 0,
+    var email: String = "",
+    var password: String = "",
+    var water: Int = 0,
+    var medicine: Int = 0,
+    var food: Int = 0,
+    var clothes: Int = 0,
+    var constructionMaterials: Int = 0
+
+
+)
