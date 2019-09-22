688e32c340bd3e77a7bade106b815034f23ef93f
Cristian Caro <ccaro98>
2019-09-22 04:31:21 -0500

    payment info

--- app/src/main/java/cc/captaincode/hurrileaf/activities/paymentDonation/ui/PaymentDonationActivity.kt
+++ app/src/main/java/cc/captaincode/hurrileaf/activities/paymentDonation/ui/PaymentDonationActivity.kt
@@ -3,11 +3,37 @@ package cc.captaincode.hurrileaf.activities.paymentDonation.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import cc.captaincode.hurrileaf.R
+import cc.captaincode.hurrileaf.activities.paymentDonation.viewModel.PaymentDonationViewModel
+import cc.captaincode.hurrileaf.data.PaymentModel
+
+import kotlinx.android.synthetic.main.activity_payment_donation.*
 
 class PaymentDonationActivity : AppCompatActivity() {
 
+    lateinit var paymentDonationViewModel: PaymentDonationViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_payment_donation)
+
+        var value = intent.extras?.getString("resourceCategory")!!
+        tvCategoryLabel.text = value.capitalize()
+        paymentDonationViewModel = PaymentDonationViewModel(this)
+
+        btnConfirmPayment.setOnClickListener {
+            savePaymentInfo()
+        }
+
     }
+
+    fun savePaymentInfo() {
+        paymentDonationViewModel.savePaymentInfo(
+            PaymentModel(
+                Integer.parseInt(etPaymentAmount.text.toString()).toDouble(),
+                tvCategoryLabel.text.toString(),
+                etCardHolder.text.toString()
+            )
+        )
+
+    }
+
 }
---
+++ app/src/main/java/cc/captaincode/hurrileaf/data/PaymentModel.kt
@@ -0,0 +1,7 @@
+package cc.captaincode.hurrileaf.data
+
+data class PaymentModel (
+    var amount: Double = 0.0,
+    var category: String = "water",
+    var name: String = ""
+    )
